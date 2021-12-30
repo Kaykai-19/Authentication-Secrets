43f1f68cd0b16dc43a968ec2c04e28b3aa47c076
Charles Adeyinka <ike.adeyinka@gmail.com>
2021-12-29 19:05:54 -0500

    Started hashing passwords

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 //jshint esversion:6
 
 require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 
 const app = express();
 
-console.log(process.env.SECRET);
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -30,7 +29,6 @@ const userSchema = new mongoose.Schema ({
 });
 
 //encyrpt needs to be before the model
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 
 // mongoose model
@@ -51,7 +49,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -65,7 +63,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if (err){
